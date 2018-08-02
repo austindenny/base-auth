ba0e22a2e1d9da0640c8730c51ef9620a66b3cbe
Austin Denny <austindenny@Austins-MacBook-Pro.local>
2018-08-02 15:26:26 -0500

    necessary base controllers: users, static_pages as an example of how to apply before action, knock

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,13 @@
 class StaticPagesController < ApplicationController
+
+  before_action :authenticate_user, only: [:auth]
+
+  def home
+    render json: { service: 'auth-api', status: 200 }
+  end
+
+  def auth
+    render json: { status: 200, msg: "You are currently logged in as #{current_user.username}" }
+  end
+
 end
